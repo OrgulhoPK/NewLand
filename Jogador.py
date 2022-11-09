d36239fe40673b9f71b2f6ac0a5980a1ef51b06b
Guilherme Ramos <guilherme.silva.61@edu.ufes.br>
2022-11-08 23:22:44 -0300

    Corrigido bug, em que o personagem andava parado

--- Jogador.py
+++ Jogador.py
@@ -41,31 +41,24 @@ class Jogador:
         #tratamento da animação (desenho)
         if self.animation_count +1 >= 28:
             self.animation_count = 0
+
+        self.animation_count +=1
         
         # posição x e y, largura e altura
         x,y,l,a = self.posXY[0],self.posXY[1],self.posWH[0],self.posWH[1]
 
-        
-        if self.mov_esquerda and self.mov_direita:
-            tela.blit(pg.transform.scale(Imagem.andar[self.animation_count//28], (64,64)),(x-self.scroll[0],y-self.scroll[1]))
-            self.animation_count = 0
-            self.mov_esquerda = False
-            self.mov_direita = False    
-
-        elif not self.mov_esquerda and not self.mov_direita:
-            self.animation_count = 0
-            tela.blit(pg.transform.scale(Imagem.andar[self.animation_count//28], (64,64)),(x-self.scroll[0],y-self.scroll[1]))
-        
-        while(self.mov_direita):
-        #elif self.mov_direita:
-            tela.blit(pg.transform.scale(Imagem.andar[self.animation_count//4], (64,64)),(x-self.scroll[0],y-self.scroll[1]))         
-            self.animation_count +=1
-
-        while(self.mov_esquerda):
-        #elif self.mov_esquerda:
-            tela.blit(pg.transform.scale(pg.transform.flip(Imagem.andar[self.animation_count//4],True,False), (64,64)),(x-self.scroll[0],y-self.scroll[1]))
             
-            self.animation_count +=1
+
+        if not self.mov_esquerda and not self.mov_direita:
+            tela.blit(pg.transform.scale(Imagem.andar[0], (64,64)),(x-self.scroll[0],y-self.scroll[1]))
+        
+        elif self.mov_direita:
+            tela.blit(pg.transform.scale(Imagem.andar[self.animation_count//4], (64,64)),(x-self.scroll[0],y-self.scroll[1]))         
+            self.mov_direita = False
+        elif self.mov_esquerda:
+            tela.blit(pg.transform.scale(pg.transform.flip(Imagem.andar[self.animation_count//4],True,False), (64,64)),(x-self.scroll[0],y-self.scroll[1]))
+            self.mov_esquerda = False
+
             
         
 
