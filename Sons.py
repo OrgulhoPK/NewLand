ceccbdec71607bc24d5c752db1383b7d928d0d12
Guilherme Ramos da Silva <108903467+OrgulhoPK@users.noreply.github.com>
2022-11-21 14:16:09 -0300

    Adicionado aparencia do mob ao game

--- Sons.py
+++ Sons.py
@@ -10,7 +10,7 @@ class Sons:
 
     #Chamando a musica menu
     #som = caminho.parent / 'menu1.ogg'
-    menu1 = pg.mixer.Sound('menu1.ogg')
+    menu1 = pg.mixer.Sound('menu1.wav')
 
     
     
@@ -20,12 +20,12 @@ class Sons:
     def BarulhoProjetil(self):
         caminho = Path(__file__)
         pg.mixer.init()
-        barulho = pg.mixer.Sound('whoosh.ogg')
+        barulho = pg.mixer.Sound('whoosh.wav')
         som = barulho.play()
 
 
     def MusicaTelaInicial(self):
         
         pg.mixer.init()
-        menu1 = pg.mixer.Sound('menu1.ogg')
+        menu1 = pg.mixer.Sound('menu1.wav')
         somMenu1 = menu1.play()
