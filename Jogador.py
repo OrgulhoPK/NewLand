b6bfe7cad3f8b1d5660ba98e3674a9017d7f7b5e
Guilherme Ramos da Silva <108903467+OrgulhoPK@users.noreply.github.com>
2022-11-21 16:09:00 -0300

    mudanca pequena

--- Jogador.py
+++ Jogador.py
@@ -17,7 +17,7 @@ class Jogador:
         self.personagem = personagem
 
         #referente à animação do personagem    
-        self.anim_count = 0    #é só um contador de animação (trocar o frame)
+        self.anim_mov = 0    #é só um contador de animação (trocar o frame)
         self.mov_direita = False
         self.mov_esquerda = False
         self.mov_cima = False
@@ -83,9 +83,9 @@ class Jogador:
 
         #Contador de animação (desenho)
         if self.visible:
-            if self.anim_count +1 >= 28:
-                self.anim_count = 0
-            self.anim_count +=1
+            if self.anim_mov+1 >= 28:
+                self.anim_mov = 0
+            self.anim_mov +=1
 
             #Tratamento da animação de ataque
             if pg.mouse.get_pressed()[0]:
@@ -100,25 +100,26 @@ class Jogador:
         
                 self.countatk +=1
             
+
             if not self.atk:
                 if not self.mov_esquerda and not self.mov_direita and not self.mov_cima and not self.mov_baixo:
                     tela.blit(pg.transform.scale(baixo[0], (64,64)),(self.X,self.Y))
                 
                 elif self.mov_direita or (self.mov_direita and (self.mov_cima or self.mov_baixo)):
                     
-                    tela.blit(pg.transform.scale(esq_Dir[self.anim_count//4], (64,64)),(self.X,self.Y))
+                    tela.blit(pg.transform.scale(esq_Dir[self.anim_mov//4], (64,64)),(self.X,self.Y))
                     self.mov_direita = False
 
                 elif self.mov_esquerda or (self.mov_esquerda and (self.mov_cima or self.mov_baixo)):
-                    tela.blit(pg.transform.scale(pg.transform.flip(esq_Dir[self.anim_count//4],True,False), (64,64)),(self.X,self.Y))
+                    tela.blit(pg.transform.scale(pg.transform.flip(esq_Dir[self.anim_mov//4],True,False), (64,64)),(self.X,self.Y))
                     self.mov_esquerda = False
 
                 elif self.mov_cima:
-                    tela.blit(pg.transform.scale(cima[self.anim_count//4], (64,64)),(self.X,self.Y))  
+                    tela.blit(pg.transform.scale(cima[self.anim_mov//4], (64,64)),(self.X,self.Y))  
                     self.mov_cima = False
 
                 elif self.mov_baixo:
-                    tela.blit(pg.transform.scale(baixo[self.anim_count//4], (64,64)),(self.X,self.Y))
+                    tela.blit(pg.transform.scale(baixo[self.anim_mov//4], (64,64)),(self.X,self.Y))
                     self.mov_baixo = False
 
             #atualizar posicao do hitbox
